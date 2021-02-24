dd86c8f90e43c580be78e05fdf86e166b866bc41
comstewa <79336563+comstewa@users.noreply.github.com>
2021-02-23 17:06:49 -0800

    Update Order.c

--- pa6/Order.c
+++ pa6/Order.c
@@ -33,31 +33,18 @@ int main(int argc, char* argv[]){
 		fprintf(stderr, "Oof looks like I can't write to the file %s \n", argv[2]);
 		exit(EXIT_FAILURE);
 	}
-	//int i=0;
-	//int n=1;
-	//while(n>0){ 
-	//	n=fscanf(in,"%s",wordset[i]);
-	//	i++;
-
-	//}cor
+	
 
 	while(fgets(wordset, MAX_STRING_LENGTH, in) != NULL){	//read lines from in
-		//printf("%s\n",wordset );
-		//strcpy(betbb[count],wordset);
+		
 		count++;
-		//printf("%s\n",betbb[count] );
-
-	
-	//	printf(stderr,"%s",wordset);
+		
 	}
 
 	char *stringsA[count][MAX_STRING_LENGTH];	
-	//for(i;i>0;i--){
-	//	stringsA[i]=wordset[i];
-	//}
-	//count = 0;		
+	
 	rewind(in);								//reset wordCount
-	//while(fgets(wordset, MAX_STRING_LENGTH, in) != NULL){
+	
 	int yo=0;
 	while(fgets (wordset, MAX_STRING_LENGTH, in) != NULL){
 		char *pos;
@@ -77,87 +64,26 @@ int main(int argc, char* argv[]){
 
     	insert(A, stringsA[i], NULL);
   	}
-  	//fprintf(out,"PRE\n" );
+ 
   	
   	printDictionary(out, A, "pre");
   	fprintf(out,"\n");
   	fprintf(out,"\n");
 
-  	//fprintf(out,"IN\n" );
   	printDictionary(out, A, "in");
   	fprintf(out,"\n");
   	fprintf(out,"\n");
   	
-  //	fprintf(out,"POST\n" );
+
   	printDictionary(out, A, "post");
 
-	 //for(x=beginForward(A); currentVal(A)!=VAL_UNDEF; x=next(A)){
-	 	//y=currentKey(A);
-    //  fprintf(out,KEY_FORMAT"\n", y);
-   //}	
+
    freeDictionary(&A);
-	//}
-	//from here on out gotta code new stuff!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-
-
-/*
-
-	append(outlist,0);
-	//moveFront(outlist);
-	int r=count;
-
-	for(int i=1;i<r;i++){
-		moveFront(outlist);
-		
-		int y=0;
-		while (y==0){
-			
-			if((strcmp(stringsA[i],stringsA[get(outlist)])<=0)){
-				insertBefore(outlist,i);
-				
-				y++;
-			}if(get(outlist)==-1){
-				append(outlist,i);
-				
-				y++;
-			}
-			moveNext(outlist);
 	
 
-		}
-		//while((strcmp(stringsA[i],stringsA[index(outlist)])<=0)&&y<length(outlist)){
-		//	moveNext(outlist);
-		//	y++;
-		//}
-		//insertAfter(outlist,i);
 
-		//printf("%s\n",stringsA[i] );
 
-		//int p=0;
-		//while(p==0){
-		//for(int q=0; q<length(outlist);q++){
-		//	if((strcmp(stringsA[i],stringsA[get(outlist)])<=0)){
-		//		insertBefore(outlist,i);
-		//		p=1;
-		//	}else if(stringsA[i+1]==NULL){
-		//		prepend(outlist,i);
-		//		p=1;
-		//	}else{
-		//		moveNext(outlist);
-		//	}
 
-		//}
-	}
-	//printList(out,outlist);
-	moveFront(outlist);
-	for(int i = 0 ; i<length(outlist) ; i++){
-		fprintf(out, "%s", stringsA[(get(outlist))]);	//print corresponding string
-		moveNext(outlist);				//move cursor to next node
-	}
-
-*/
 	fclose(in);
 	fclose(out);			//close out of files
 
